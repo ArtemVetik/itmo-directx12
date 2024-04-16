a628f2b2955367ecbd8ed32b00346aa3e8da1bf8
Artem Vetik <av.unitygames@gmail.com>
2024-04-16 23:59:21 +0300

    Rendering loaded mesh with texture

--- KatamariDamacy/Shaders/color.hlsl
+++ KatamariDamacy/Shaders/color.hlsl
@@ -1,19 +1,24 @@
+Texture2D 		gDiffuseMap : register(t0);
+SamplerState 	gsamLinear  : register(s0);
+
 cbuffer cbPerObject : register(b0)
 {
 	float4x4 gWorldViewProj;
-	float4 offset; 
+    float4x4 gTexTransform;
 };
 
 struct VertexIn
 {
 	float3 PosL  : POSITION;
     float4 Color : COLOR;
+	float2 TexC  : TEXCOORD;
 };
 
 struct VertexOut
 {
 	float4 PosH  : SV_POSITION;
     float4 Color : COLOR;
+	float2 TexC  : TEXCOORD;
 };
 
 VertexOut VS(VertexIn vin)
@@ -21,17 +26,22 @@ VertexOut VS(VertexIn vin)
 	VertexOut vout;
 	
 	// Transform to homogeneous clip space.
-	vout.PosH = mul(float4(vin.PosL + offset.xyz, 1.0f), gWorldViewProj);
+	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
 	
 	// Just pass vertex color into the pixel shader.
     vout.Color = vin.Color;
     
+	float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, gTexTransform).xy;
+    
+	//vout.TexC = vin.TexC;
+	
     return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-    return pin.Color;
+	return gDiffuseMap.Sample(gsamLinear, pin.TexC);
 }
 
 
